3d7319a3d48a9f21bd4ab4ca259fa45bde3aa02c
qhjpr <hh272013@gmail.com>
2022-04-14 09:14:34 -0400

    update the Haijing1stClass

--- src/CodingBatWarmup1/Haijing1stClass.java
+++ src/CodingBatWarmup1/Haijing1stClass.java
@@ -3,5 +3,6 @@ package CodingBatWarmup1;
 public class Haijing1stClass {
     public static void main(String[] args) {
         System.out.println("made a class");
+        System.out.println("update it");
     }
 }
