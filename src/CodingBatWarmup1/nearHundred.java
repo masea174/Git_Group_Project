2d52a1107f01e4e3f90d9f56d40da1941a6dc3f4
davidlomakin <davidlomakin@yahoo.com>
2022-04-14 17:08:55 -0400

    exercises at Warmup 2 more

---
+++ src/CodingBatWarmup1/nearHundred.java
@@ -0,0 +1,15 @@
+package CodingBatWarmup1;
+
+public class nearHundred {
+    //Given an int n, return true if it is within 10 of 100 or 200.
+    // Note: Math.abs(num) computes the absolute value of a number.
+    public boolean nearHundred(int n) {
+if (n >= 90 && n <= 110){
+    return true;
+}else if(n >= 190 && n <= 210){
+    return true;
+}else{
+    return false;
+}
+    }
+}
