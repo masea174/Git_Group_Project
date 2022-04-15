a566f6c11fb94e9f1578ea988a05705c70a6ac9e
davidlomakin <davidlomakin@yahoo.com>
2022-04-15 18:12:27 -0400

    Warmup 1

---
+++ src/CodingBatWarmup1/notString.java
@@ -0,0 +1,14 @@
+package CodingBatWarmup1;
+
+public class notString {
+   // Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not",
+    // return the string unchanged. Note: use .equals() to compare 2 strings.
+   public String notString(String str) {
+if (str.startsWith("not")){
+    return str;
+}else{
+    return "not " + str;
+}
+   }
+
+}
