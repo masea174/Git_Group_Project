f014cd7cf4b723e7a2538144ddc1edaebce50e18
Branch <krische356@gmail.com>
2022-04-21 09:43:27 -0400

    created a class withoutEnd

---
+++ src/String1/withoutEnd.java
@@ -0,0 +1,13 @@
+package String1;
+
+public class withoutEnd {
+    /*
+    Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+     */
+
+    public String withoutEnd(String str) {
+
+        return str.substring(1, str.length()-1);
+    }
+
+}
